913fe5074759e5ae6d26f53034225c25d7bf28b6
Zidan016-pens <107998599+Zidan016-pens@users.noreply.github.com>
2024-12-17 11:01:10 +0700

    update pencatatan, unit, ruang, aset, peroleh, dll kecuali riwayat dan akun

--- backend/db.js
+++ backend/db.js
@@ -201,7 +201,7 @@ const getAllMerk = async ()=>{
 
 const getMerkId = async(id)=>{
     const db = await koneksiDB();
-    const sql = `SELECT * FORM merk where idMerk = '${id}'`
+    const sql = `SELECT * FROM merk WHERE idMerk = '${id}'`
     try {
         const [rows] = await db.execute(sql)
         return rows.length > 0 ? rows : false
@@ -260,17 +260,17 @@ const getAllMasset = async () =>{
     }
 }
 
-const getMassetId = async (id) =>{
-    const db = await koneksiDB()
-    const sql = `SELECT * FROM masteraset WHERE idMasteraset = '${id}'`
+const getMassetById = async (id) => {
+    const db = await koneksiDB();
+    const sql = `SELECT * FROM masteraset WHERE idMasteraset = ?`;
     try {
-        const [data] = await db.execute(sql)
-        return data.length > 0 ? data : false
+        const [data] = await db.execute(sql, [id]);
+        return data.length > 0 ? data[0] : null;
     } catch (e) {
-        console.log(`ada error : ${e}`)
-        return 400;
+        console.error(`Ada error: ${e}`);
+        return null;
     }
-}
+};
 
 const getMassetIdMerk = async (id) =>{
     const db = await koneksiDB()
@@ -299,7 +299,7 @@ const getMassetIdKategori = async (id)=>{
 const addMasset = async (tipe, spesifikasi, deskripsi, idKategori, idMerk, idUser) =>{
     const id = `AST${crypto.randomBytes(3).toString('hex')}`
     const db = await koneksiDB()
-    const sql = `INSERT INTO masteraset VALUES ('${id}', '${tipe}', '${spesifikasi}', '${deskripsi}', '${idKategori}', '${idMerk}', NOW(), NOW(), '${idUser}', '${idUser}')`
+    const sql = `INSERT INTO masteraset VALUES ('${id}', '${tipe}', '${spesifikasi}', '${deskripsi}', "${idKategori}", "${idMerk}", NOW(), NOW(), '${idUser}', '${idUser}')`
     try {
         await db.execute(sql)
         return 200
@@ -311,7 +311,7 @@ const addMasset = async (tipe, spesifikasi, deskripsi, idKategori, idMerk, idUse
 
 const updateMasset = async (id, tipe, spesifikasi, deskripsi, idKategori, idMerk, idUser) =>{
     const db = await koneksiDB()
-    const sql = `UPDATE masteraset tipe = ${tipe}, spesifikasi = ${spesifikasi}, deskripsi = ${deskripsi}, idKategori = ${idKategori}, idMerk = ${idMerk}, tglUpdate = NOW(), idUpdate = '${idUser}' WHERE idMasteraset = ${id}`
+    const sql = `UPDATE masteraset SET tipe = '${tipe}', spesifikasi = '${spesifikasi}', deskripsi = '${deskripsi}', idKategori = '${idKategori}', idMerk = '${idMerk}', tglUpdate = NOW(), idUpdate = '${idUser}' WHERE idMasteraset = '${id}'`
     try {
         await db.execute(sql)
         return 200
@@ -336,7 +336,7 @@ const delMasset = async(id)=>{
 const addUnit = async (nama, idUser)=>{
     const db = await koneksiDB()
     const id = `UNT${crypto.randomBytes(3).toString('hex')}`
-    const sql = `INSERT INTO unit VALUES('${id}', '${nama}', NOW(), NOW(), '${idUser}', ${idUser})`
+    const sql = `INSERT INTO unit VALUES('${id}', '${nama}', NOW(), NOW(), '${idUser}', '${idUser}')`
     try {
         await db.execute(sql)
         return 200;
@@ -394,5 +394,207 @@ const deleteUnit = async (id)=>{
     }
 }
 
+const addRuang = async (nama, idUnit, idUser)=>{
+    const db = await koneksiDB()
+    const id = `RNG${crypto.randomBytes(3).toString('hex')}`
+    const sql = `INSERT INTO ruang VALUES('${id}', '${nama}', '${idUnit}', NOW(), NOW(), '${idUser}', '${idUser}')`
+    try {
+        await db.execute(sql)
+        return 200;
+    } catch (e) {
+        console.log(`ada error : ${e}`)
+        return 400;
+    }
+}
 
-module.exports = {koneksiDB, addLevel, updateLevel, getAllLevel, addAkses, updateAkses, getAksesByNama, getAllAkses, getAllKategori, getKategoriId, addKategori, updateKategori, getAllMerk, getMerkId, addMerk, updateMerk, delAkses, delKategori, delLevel, addMerk, getAllMasset, getMassetIdMerk, getMassetIdKategori, addMasset, updateMasset, delMasset, getMassetId, delMerk, getAllUnit, getUnitById, addUnit, deleteUnit, updateUnit} 
+const getAllRuang = async ()=>{
+    const db = await koneksiDB()
+    const sql = `SELECT * FROM ruang`
+    try {
+        const [data] = await db.execute(sql)
+        return data.length > 0 ? data : []
+    } catch (e) {
+        console.log(`ada error : ${e}`)
+        return 400;
+    } 
+}
+
+const getRuangById = async (id)=>{
+    const db = await koneksiDB()
+    const sql = `SELECT FROM ruang WHERE idRuang = '${id}'`
+    try {
+        const data = await db.execute(sql)
+        return data.length > 0 ? data : false
+    } catch (e) {
+        console.log(`ada error : ${e}`)
+        return 400;
+    }
+}
+
+const updateRuang = async (id, nama, idUnit,idUser)=>{
+    const db = await koneksiDB()
+    const sql = `UPDATE ruang SET nama = '${nama}', idUnit = '${idUnit}', tglUpdate = NOW(), idUpdate = '${idUser}' WHERE idRuang = '${id}'`
+    try {
+        await db.execute(sql)
+        return 200;
+    } catch (e) {
+        console.log(`ada error : ${e}`)
+        return 400;
+    }
+}
+
+const delRuang = async (id)=>{
+    const db = await koneksiDB()
+    const sql = `DELETE FROM ruang WHERE idRuang  = '${id}'`
+    try {
+        await db.execute(sql)
+        return 200;
+    } catch (e) {
+        console.log(`ada error : ${e}`)
+        return 400;
+    }
+}
+
+const addPerolehan = async (nama, keterangan, idUser)=>{
+    const db = await koneksiDB()
+    const id = `PRL${crypto.randomBytes(5).toString('hex')}`
+    const sql = `INSERT INTO perolehan VALUES('${id}', '${nama}', '${keterangan}', NOW(), NOW(), '${idUser}', '${idUser}')`
+    try {
+        await db.execute(sql)
+        return 200;
+    } catch (e) {
+        console.log(`ada error : ${e}`)
+        return 400;
+    }
+}
+
+const getAllPerolehan = async ()=>{
+    const db = await koneksiDB()
+    const sql = `SELECT * FROM perolehan`
+    try {
+        const [data] = await db.execute(sql)
+        return data.length > 0 ? data : []
+    } catch (e) {
+        console.log(`ada error : ${e}`)
+        return 400;
+    } 
+}
+
+const getPerolehanById = async (id) => {
+    const db = await koneksiDB();
+    const sql = `SELECT * FROM perolehan WHERE idPerolehan = ?`;
+    try {
+        const [data] = await db.execute(sql, [id]);
+        return data.length > 0 ? data[0] : null;
+    } catch (e) {
+        console.error(`Ada error: ${e}`);
+        return null;
+    }
+}
+
+
+const updatePerolehan = async (id, nama, keterangan, idUser)=>{
+    const db = await koneksiDB()
+    const sql = `UPDATE perolehan SET nama = '${nama}', keterangan = '${keterangan}', tanggalUpdate = NOW(), idUpdate = '${idUser}' WHERE idPerolehan = '${id}'`
+    try {
+        await db.execute(sql)
+        return 200;
+    } catch (e) {
+        console.log(`ada error : ${e}`)
+        return 400;
+    }
+}
+
+const delPerolehan = async (id)=>{
+    const db = await koneksiDB()
+    const sql = `DELETE FROM perolehan WHERE idPerolehan  = '${id}'`
+    try {
+        await db.execute(sql)
+        return 200;
+    } catch (e) {
+        console.log(`ada error : ${e}`)
+        return 400;
+    }
+}
+
+const addPencatatan = async (idAsset, idPerolehan, harga, tglPeroleh, keterangan, status, idUser)=>{
+    const db = await koneksiDB()
+    const id = `PCT${crypto.randomBytes(5).toString('hex')}`
+    const sql = `INSERT INTO pencatatan VALUES('${id}', '${idAsset}', '${idPerolehan}', '${harga}', '${tglPeroleh}', '${keterangan}', '${status}', NOW(), NOW(), '${idUser}', '${idUser}')`
+    try {
+        await db.execute(sql)
+        return 200;
+    } catch (e) {
+        console.log(`ada error : ${e}`)
+        return 400;
+    }
+}
+
+const updatePencatatan = async (idAsset, idPerolehan, harga, tglPeroleh, keterangan, status, idUser, id) => {
+    const db = await koneksiDB()
+    const sql = `
+        UPDATE pencatatan 
+        SET 
+            idMasteraset = '${idAsset}', 
+            idPerolehan = '${idPerolehan}', 
+            harga = '${harga}', 
+            tglPerolehan = '${tglPeroleh}', 
+            keterangan = '${keterangan}', 
+            status = '${status}', 
+            idUpdate = '${idUser}', 
+            tglUpdate = NOW()
+        WHERE 
+            idPencatatan = '${id}'
+    `;
+    try {
+        await db.query(sql);
+        return 200;
+    } catch (error) {
+        console.log(error);
+        return 400;
+    }
+}
+
+const getAllPencatatan = async () => {
+    const db = await koneksiDB()
+    const sql = `SELECT * FROM pencatatan`;
+    try {
+        const [data] = await db.execute(sql)
+        return data.length > 0 ? data : []
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+const getByIdPencatatan = async (id) => {
+    const db = await koneksiDB()
+    const sql = `SELECT * FROM pencatatan WHERE idPencatatan = '${id}'`;
+    try {
+        const data = await db.execute(sql)
+        return data.length > 0 ? data : []
+    } catch (error) {
+        console.error(`Error fetching pencatatan with id ${id}:`, error);
+        return [];
+    }
+}
+
+const delPencatatan = async (id)=>{
+    const db = await koneksiDB()
+    const sql = `DELETE FROM pencatatan WHERE idPencatatan  = '${id}'`
+    try {
+        await db.execute(sql)
+        return 200;
+    } catch (e) {
+        console.log(`ada error : ${e}`)
+        return 400;
+    }
+}
+
+const addRiwayat = async (idPencatatan, idRuang, kondisi, tglAudit)=>{
+    const db = await koneksiDB()
+    const id = `RYT${crypto.randomBytes(5).toString('hex')}`
+    const sql = `INSERT INTO riwayat VALUES('${id}', '${idPencatatan}', '${kondisi}', '${idRuang}', '${tglAudit}', NOW(), NOW())`
+}
+
+module.exports = {koneksiDB, addLevel, updateLevel, getAllLevel, addAkses, updateAkses, getAksesByNama, getAllAkses, getAllKategori, getKategoriId, addKategori, updateKategori, getAllMerk, getMerkId, addMerk, updateMerk, delAkses, delKategori, delLevel, addMerk, getAllMasset, getMassetIdMerk, getMassetIdKategori, addMasset, updateMasset, delMasset, getMassetById, delMerk, getAllUnit, getUnitById, addUnit, deleteUnit, updateUnit, addRuang, getAllRuang, getRuangById, updateRuang, delRuang, addPerolehan, getAllPerolehan, getPerolehanById, delPerolehan, updatePerolehan, addPencatatan, updatePencatatan, delPencatatan, getAllPencatatan, getByIdPencatatan} 
--- myApp.js
+++ myApp.js
@@ -4,7 +4,8 @@ const { initDB } = require('./backend/initilizing');
 const app = express();
 const session = require('express-session')
 const idAdmin = "LyPiw";
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const { name } = require('ejs');
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -20,18 +21,26 @@ app.use(session({
     }}
 ));
 app.use((req, res, next) => {
-     if (!req.session.role) {
+     if (!req.session.role && !req.session.idUser) {
         req.session.role = 'guest'; 
+        req.session.idUser = 'guest'
     }
     next();
 })
+
+function checkSession(req, res, next){
+    if(req.session.role == 'guest' && req.session.idUser == 'guest'){
+        return res.redirect('/')
+    }
+    next()
+}
 
 function checkRole(role) { 
     return function (req, res, next) { 
         if (req.session.role && req.session.role === role) {
              next(); 
         } else { 
-            res.status(403).send('Akses ditolak: Anda tidak memiliki izin yang cukup'); 
+            res.redirect("/dashboard") 
         } 
     };
 }
@@ -72,17 +81,48 @@ app.post("/login", async (req, res) => {
 });
 
 
-app.get("/dashboard", async (req, res)=>{
-    // res.render("index", {namafile: "dashboard"})
-    return res.send({message : `${req.session.role}, ${req.session.idUser}`});
+app.get("/dashboard", checkSession, async (req, res)=>{
+    res.render("index", {namafile: "dashboard"})
 })
 
-app.get("/dataMaster", async (req, res)=>{
+app.get("/dataMaster", checkSession, async (req, res)=>{
     const mAset = await db.getAllMasset();
-    res.render("index", {namafile: "dataMaster", data: mAset})
+    const kategori = await db.getAllKategori()
+    const merk = await db.getAllMerk()
+    res.render("index", {namafile: "dataMaster", data: mAset, dKategori : kategori, dMerk: merk})
 })
 
-app.post("/addLevel", async (req, res)=>{
+app.post("/addAsset",checkSession, async(req, res)=>{
+    const {tipe, spesifikasi, deskripsi, idKategori, idMerk} = req.body
+    try {
+        await db.addMasset(tipe, spesifikasi, deskripsi, idKategori, idMerk, req.session.idUser)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500)
+    }
+})
+
+app.put("/editAsset", checkSession, async(req, res)=>{
+    const {id, tipe, spesifikasi, deskripsi, idKategori, idMerk} = req.body
+    try {
+        await db.updateMasset(id, tipe, spesifikasi, deskripsi, idKategori, idMerk, req.session.idUser)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500)
+    }
+})
+
+app.delete("/deleteAsset", checkSession, async(req, res)=>{
+    const {id} = req.body
+    try {
+        await db.delMasset(id)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500)
+    }
+})
+
+app.post("/addLevel",checkSession, async (req, res)=>{
      try {
         const {nama} = req.body;
         await db.addLevel(nama, idUser, idUser)
@@ -92,7 +132,7 @@ app.post("/addLevel", async (req, res)=>{
      }
 })
 
-app.put("/updateLevel", async(req, res)=>{
+app.put("/updateLevel",checkSession, async(req, res)=>{
     try {
         const {idLevel, namaBaru} = req.body;
         await db.updateLevel(idLevel, namaBaru, idUser)
@@ -102,20 +142,16 @@ app.put("/updateLevel", async(req, res)=>{
     }
 })
 
-app.get("/user", async (req, res)=>{
+app.get("/akun",checkSession, async (req, res)=>{
     try {
         const rows = await db.getAllAkses()
-        if(rows != false){
-            res.send(rows)
-        }else{
-            res.send({message : 'Tidak Ada User'})
-        }
+        res.render("index", {namafile: "dataMaster", data: rows})
     } catch (e) {
         res.send({message : `${e.message}`})       
     }
 })
 
-app.post("/addUser", async(req, res)=>{
+app.post("/addUser",checkSession, async(req, res)=>{
     const {nama, username, password, status} = req.body
     try {
         const respone = await db.addAkses(nama, username, password, status, idAdmin, idUser, idUser)
@@ -129,7 +165,7 @@ app.post("/addUser", async(req, res)=>{
     }
 })
 
-app.put("/updateUser", async(req, res)=>{
+app.put("/updateUser",checkSession, async(req, res)=>{
     const {idAkses, nama, username, password, status, idLevel} = req.body
     try {
         const respone = await db.updateAkses(idAkses, nama, username, password, status, idLevel, idUser, idUser)
@@ -143,7 +179,7 @@ app.put("/updateUser", async(req, res)=>{
     }
 })
 
-app.get("/merk", async(req, res)=>{
+app.get("/merk",checkSession, async(req, res)=>{
     try {
         const respone = await db.getAllMerk();
         res.render("index", {namafile: "merk", data: respone})
@@ -152,7 +188,7 @@ app.get("/merk", async(req, res)=>{
     }
 })
 
-app.get("/merkId/:id", async(req, res)=>{
+app.get("/merkId/:id",checkSession, async(req, res)=>{
     const id = req.params.id
     try {
         const respone = await db.getMerkId(id);
@@ -166,24 +202,20 @@ app.get("/merkId/:id", async(req, res)=>{
     }
 })
 
-app.post("/addMerk", async(req, res)=>{
+app.post("/addMerk",checkSession, async(req, res)=>{
     const {nama} = req.body
     try {
-        const respone = await db.addMerk(nama, idUser)
-        if(respone == 200){
-            res.send({message : 'Berhasil Insert'})
-        }else{
-            res.send({message : 'Ada error pada insert'})
-        }
+        await db.addMerk(nama, req.session.idUser)
+        res.sendStatus(200)
     } catch (e) {
         res.send({message : `${e.message}`})
     }
 })
 
-app.put("/updateMerk", async(req, res)=>{
+app.put("/updateMerk",checkSession, async(req, res)=>{
     const {nama, id} = req.body
     try {
-        const respone = await db.updateMerk(nama, idUser, id)
+        const respone = await db.updateMerk(nama, req.session.idUser, id)
         if(respone == 200){
             res.send({message : 'Berhasil Insert'})
         }else{
@@ -194,7 +226,7 @@ app.put("/updateMerk", async(req, res)=>{
     }
 })
 
-app.delete("/deleteMerk", async (req, res)=>{
+app.delete("/deleteMerk",checkSession, async (req, res)=>{
     const {id} = req.body
     try {
         await db.delMerk(id)
@@ -204,7 +236,7 @@ app.delete("/deleteMerk", async (req, res)=>{
     }
 })
 
-app.get("/kategori", async(req, res)=>{
+app.get("/kategori",checkSession, async(req, res)=>{
     try {
         const respone = await db.getAllKategori();
         res.render("kategori", {namafile : "kategori", data : respone})
@@ -213,7 +245,7 @@ app.get("/kategori", async(req, res)=>{
     }
 })
 
-app.get("/kategoriId/:id", async(req, res)=>{
+app.get("/kategoriId/:id",checkSession,  async(req, res)=>{
     const id = req.params.id
     try {
         const respone = await db.getKategoriId(id);
@@ -227,10 +259,10 @@ app.get("/kategoriId/:id", async(req, res)=>{
     }
 })
 
-app.post("/addKategori", async(req, res)=>{
+app.post("/addKategori", checkSession, async(req, res)=>{
     const {nama} = req.body
     try {
-        const respone = await db.addKategori(nama, idUser)
+        const respone = await db.addKategori(nama, req.session.idUser)
         if(respone == 200){
             res.send({message : 'Berhasil Insert'})
         }else{
@@ -241,10 +273,10 @@ app.post("/addKategori", async(req, res)=>{
     }
 })
 
-app.put("/updateKategori", async(req, res)=>{
+app.put("/updateKategori", checkSession, async(req, res)=>{
     const {nama, id} = req.body
     try {
-        const respone = await db.updateKategori(id, nama, idUser)
+        const respone = await db.updateKategori(id, nama, req.session.idUser)
         if(respone == 200){
             res.send({message : 'Berhasil Update'})
         }else{
@@ -255,7 +287,171 @@ app.put("/updateKategori", async(req, res)=>{
     }
 })
 
-const PORT = process.env.PORT || 3333;
+app.delete("/deleteKategori", checkSession, async(req, res)=>{
+    const {id} = req.body
+    try {
+        await db.delKategori(id)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500)
+    }
+})
+
+app.get("/unit", checkSession, async(req, res)=>{
+    const data = await db.getAllUnit()
+    res.render("index", {namafile : "unit", data : data})
+})
+
+app.post("/addUnit", checkSession, async(req, res)=>{
+    const {nama} = req.body;
+    try {
+        await db.addUnit(nama, req.session.idUser)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500)
+    }
+})
+
+app.put("/updateUnit", checkSession, async(req, res)=>{
+    const {nama, id} = req.body
+    try {
+        await db.updateUnit(id, nama, req.session.idUser)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500)
+    } 
+})
+
+app.delete("/deleteUnit", checkSession, async(req, res)=>{
+    const {id} = req.body
+    try {
+        await db.deleteUnit(id)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500)
+    }
+})
+
+app.get("/ruang", checkSession, async(req,res)=>{
+    const data = await db.getAllRuang()
+    const unit = await db.getAllUnit()
+    res.render("index", {namafile : "ruang", data: data, unit : unit}) 
+})
+
+app.post("/addRuang", checkSession, async(req, res)=>{
+    const {nama, idUnit} = req.body
+    try {
+        await db.addRuang(nama, idUnit, req.session.idUser)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500)
+    }
+})
+
+app.put("/updateRuang", checkSession, async(req, res)=>{
+    const {nama, idUnit, id} = req.body
+    try {
+        await db.updateRuang(id, nama, idUnit, req.session.idUser)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500)
+    }
+})
+
+app.delete("/deleteRuang", checkSession, async(req, res)=>{
+    const {id} = req.body
+    try {
+        await db.delRuang(id)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500)
+    }
+})
+
+app.get("/perolehan", checkSession, async(req, res)=>{
+    const data = await db.getAllPerolehan()
+    res.render("index", {namafile : "perolehan", data : data})
+})
+
+app.post("/addPerolehan", checkSession, async(req, res)=>{
+    const {nama, keterangan} = req.body
+    try {
+        await db.addPerolehan(nama, keterangan, req.session.idUser)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500)
+    }
+})
+
+app.put("/updatePerolehan", checkSession, async(req, res)=>{
+    const {nama, keterangan, id}= req.body
+    try {
+        await db.updatePerolehan(id, nama, keterangan, req.session.idUser)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500) 
+    }
+})
+
+app.delete("/deletePerolehan", checkSession, async (req, res)=>{
+    const {id} = req.body
+    try {
+        await db.delPerolehan(id)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500) 
+    }
+})
+
+app.get("/pencatatan", checkSession, async(req, res)=>{
+    const data = await db.getAllPencatatan()
+    const aset = await db.getAllMasset()
+    const peroleh = await db.getAllPerolehan()
+    res.render("index", {namafile:"pencatatan", data: data, aset : aset, peroleh : peroleh})
+})
+
+app.post("/detailPencatatan", checkSession, async(req, res)=>{
+    const {idPeroleh, idAsset} = req.body
+    try {
+        const peroleh = await db.getPerolehanById(idPeroleh)
+        const asset = await db.getMassetById(idAsset)
+        res.status(200).json({"peroleh": peroleh, "asset": asset})
+    } catch (e) {
+        res.sendStatus(500) 
+    }
+})
+
+app.post("/addPencatatan", checkSession, async(req, res)=>{
+    const {idAsset, idPerolehan, harga, tglPeroleh, keterangan, status}= req.body
+    try {
+        await db.addPencatatan(idAsset, idPerolehan, harga, tglPeroleh, keterangan, status, req.session.idUser)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500) 
+    }
+})
+
+app.put("/updatePencatatan", checkSession, async(req, res)=>{
+    const {idAsset, idPerolehan, harga, tglPeroleh, keterangan, status, id} = req.body
+    try {
+        await db.updatePencatatan(idAsset, idPerolehan, harga, tglPeroleh, keterangan, status, req.session.idUser, id)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500) 
+    }
+})
+
+app.delete("/deletePencatatan", checkSession, async(req, res)=>{
+    const {id}= req.body
+    try {
+        await db.delPencatatan(id)
+        res.sendStatus(200)
+    } catch (e) {
+        res.sendStatus(500) 
+    }
+})
+
+// const PORT = process.env.PORT || 3333;
 app.listen(3333, () => {
     console.log("Web running : " + 3333);
 })
